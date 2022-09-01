52b2042af0b25ae931a0cb7ae875e9aea568467b
Rajat <rajatdoijad67@gmail.com>
2022-09-01 15:42:33 +0530

    feat: done chagne

--- pages/employee.vue
+++ pages/employee.vue
@@ -14,7 +14,7 @@
                 class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 for="id"
               >
-                Employee id
+                Employee Id
               </label>
               <input
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500"
