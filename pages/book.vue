2d643e166cb4161c2c63a77d2480748c65ed3d93
Vishal Walekar <vishalwalekar123@gmail.com>
2022-09-02 15:07:08 +0530

    feat: add second table

--- pages/book.vue
+++ pages/book.vue
@@ -5,79 +5,70 @@
         <div >
           <div class="text-center">
             
-            <form method="post" class="flex justify-center bg-blue-300 px-12" autocomplete="off">
+            <form method="post" class="flex justify-center bg-blue-100 px-12" autocomplete="off">
             <table>
-              <div class="px-2">
-              <label class="font-bold">Database Error</label><hr/><br/>
-              <label>Book Id Error___________</label><br/>
-              <label>Book Name Error___________</label><br/>
-              <label>Book Author Error___________</label><br/>
-              <label>Book price Error___________</label><br/>
-              <label>Book Image Error___________</label><br/>
-              <label>Book ISBN Number Error___________</label><br/>
-              </div>
                 <hr/><h2 class="text-teal-900 text-xl font-bold pt-6 text-center">Add a Book To Libaray</h2>
                 <hr/>
                  <br/>
                 <label class="pt-10 py-10 mt-4" for="id">Book Id :- </label>
-                <input type="number"  id="id" name="id" placeholder="" v-model="formData.book_id" @change="validationId"/><br/>
+                <input class=" border-black rounded-md border-2" type="number"  id="id" name="id" placeholder="" v-model="formData.book_id" @change="validationId"/><br/>
                 <span class="text-sm text-red-500" v-for="error in v$.book_id.$errors" :key="error.$uid">{{error.$message}}</span>
                 <br/><br/>
                 
                 <label for="book_name">Book Name:- </label>
-                <input type="text"  id="book_name" name="book_name" placeholder="" v-model="formData.book_name" @change="validationName"/><br/>
+                <input class=" border-black rounded-md border-2" type="text"  id="book_name" name="book_name" placeholder="" v-model="formData.book_name" @change="validationName"/><br/>
                 <span class="text-sm text-red-500" v-for="error in v$.book_name.$errors" :key="error.$uid">{{error.$message}}</span>
                 <br/><br/>
                 
                 <label for="book_author">Book Author:- </label>
-                <input type="text"  id="book_author" name="book_author" placeholder="" v-model="formData.book_author" @change="validationAuthor"><br/>
+                <input class=" border-black rounded-md border-2" type="text"  id="book_author" name="book_author" placeholder="" v-model="formData.book_author" @change="validationAuthor"><br/>
                 <span class="text-sm text-red-500" v-for="error in v$.book_author.$errors" :key="error.$uid">{{error.$message}}</span>
                 <br><br>
                 
                 <label for="book_price">Book Price:- </label>
-                <input type="number"  id="book_price" name="book_price" placeholder="" v-model="formData.book_price" @change="validationPrice"><br/>
+                <input class=" border-black rounded-md border-2" type="number"  id="book_price" name="book_price" placeholder="" v-model="formData.book_price" @change="validationPrice"><br/>
                 <span class="text-sm text-red-500" v-for="error in v$.book_price.$errors" :key="error.$uid">{{error.$message}}</span>
                 <br><br/>
                 
                 <label for="book_image">Book Image:- </label>
-                <input type="text"  id="book_image" name="book_image" placeholder="" v-model="formData.book_image" @change="validationImage"/><br/>
+                <input class=" border-black rounded-md border-2"  type="text"  id="book_image" name="book_image" placeholder="" v-model="formData.book_image" @change="validationImage"/><br/>
                 <span class="text-sm text-red-500" v-for="error in v$.book_image.$errors" :key="error.$uid">{{error.$message}}</span>
                 <br/><br/>
                
                 <label for="book_isbn">Book ISbn:- </label>
-                <input type="text"  id="book_isbn" name="book_isbn" placeholder="" v-model="formData.book_isbn" @change="validationIsbn"/><br/>
+                <input class=" border-black rounded-md border-2" type="text"  id="book_isbn" name="book_isbn" placeholder="" v-model="formData.book_isbn" @change="validationIsbn"/><br/>
                 <span class="text-sm text-red-500"  v-for="error in v$.book_isbn.$errors" :key="error.$uid">{{error.$message}}</span>
                 <br/><br/>
 
-                <!-- <label for="bookcategory_name">Book category Name:- </label>
-                <input type="text"  id="bookcategory_name" name="bookcategory_name" placeholder="" v-model="formData.bookcategory_name" @change="validationIsbn"/><br/>
-                <span class="text-sm text-red-500"  v-for="error in v$.bookcategory_name.$errors" :key="error.$uid">{{error.$message}}</span>
-                <br/><br/> -->
-                 <!-- <select name="book_category" id="book_category" v-model="formData.book_category">
+                <label for="bookcategory_id">Book Category ID:- </label>
+                <input class=" border-black rounded-md border-2" type="number"  id="bookcategory_id" name="bookcategory_id" placeholder="" v-model="formDatacategory.bookcategory_id" @change="validationIsbn"/><br/>
+                <span class="text-sm text-red-500"  v-for="error in vv$.bookcategory_id.$errors" :key="error.$uid">{{error.$message}}</span>
+                <br/><br/>
+
+
+                <label for="bookcategory_name">Book Category Name:- </label>
+                <!-- <input class=" border-black rounded-md border-2" type="text"  id="bookcategory_name" name="bookcategory_name" placeholder="" v-model="formDatacategory.bookcategory_name"    /><br/>
+                <span class="text-sm text-red-500"  v-for="error in vv$.bookcategory_name.$errors" :key="error.$uid">{{error.$message}}</span> -->
+                <!-- <br/><br/> -->
+                 <select name="bookcategory_name" id="bookcategory_name" v-model="formDatacategory.bookcategory_name" multiple>
                     <option value="category">Book Category</option>
                     <option value="novels">Novels</option>
-                    <option value="comics">Comics</option>
-                    <option value="scifi">Science Fiction</option>
-                    <option value="fantasy">Fantasy</option>
                     <option value="horror">Horror</option>
-                    <option value="action">Action</option>
                     <option value="adventure">Adventure</option>
-                    <option value="detective">Detective</option>
-                    <option value="mystery">Mystery</option>
                     <option value="education">Educational</option>
                     <option value="history">Historical</option>
                     <option value="geographical">Geographical</option>
                     <option value="business">Business</option>
-                    <option value="enterpreneurship">Enterpreneurship</option>
                     <option value="fitness">Body Fitness</option>
-                    <option value="food">Kitechen Food</option>
-                    <option value="spiritual">Spiritual</option>
-                </select> -->
+                </select>
                 <div class="mt-10">
                     <button class="py-1 px-5 mr-5 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3" type="submit" @click="formSubmit" >Add Book</button>
                     <button class="py-1 px-5 mr-3 bg-black hover:bg-blue-400 text-white font-bold text-center rounded-md mb-3" type="reset">Reset</button>
                     <button class="py-1 px-2 mx-3 rounded-lg bg-green-600 hover:bg-green-600 text-white w-20" @click="getalldata">
                          All Books
+                    </button>
+                     <button class="py-1 px-2 mx-3 rounded-lg bg-green-600 hover:bg-green-600 text-white w-20" @click="getalldatacategory">
+                         All Books Category
                     </button>
                 </div>
             </table>
@@ -108,48 +99,31 @@
               <td class="px-4 border-black rounded-lg border-4">{{item.book_image}}</td>
               <td class="px-4 border-black rounded-lg border-4">{{item.book_isbn}}</td>
               <!-- <td class="px-4 border-black rounded-lg border-4">{{item.bookcategory_name}}</td> -->
-
-                <!-- <td class="px-4 border-green-400 rounded-lg border-4">
-                    <img src="{{item.Image}}" alt="" srcset="">
-                </td> -->
-                <td class="px-4 border-black rounded-lg border-4">{{item.Action}}
+              <td class="px-4 border-black rounded-lg border-4">{{item.Action}}
                 <button class="mx-3 rounded-lg bg-green-600 hover:bg-green-600 text-white w-20" @click="editBook(item)">
                         Edit
                     </button>
                     <button class="mx-3 rounded-lg bg-red-600 hover:bg-red-700 text-white w-20"  @click="deletespecificuser(item.book_id)">
                         Delete
                     </button>
-            
-                </td>
+            </td>
             </tr>
         </table>
         </div>
-        <!-- search table  -->
-        <!-- <div class="text-bold px-8">
-                <label class="text-bold" for="searchbox">Search Books- </label>
-                <input class=" border-black rounded-lg border-2" @keyup="bookFind(bookaddress)" v-model="this.bookaddress" type="text" name="searchbox" id="searchbox">
-        </div>
-        <div class="border-black border-2 m-8 p-8">
-        <table class="list">
+
+          <!-- Category table  -->
+          <div class="border-black border-2 m-8 p-8 flex justify-center">
+          <table class="list">
             <tr>
-                <th class="px-4 border-black rounded-lg border-4">Book Id</th>
-                <th class="px-4 border-black rounded-lg border-4">Book Name</th>
-                <th class="px-4 border-black rounded-lg border-4">Book Author</th>
-                <th class="px-4 border-black rounded-lg border-4">Book Image</th>
-                <th class="px-4 border-black rounded-lg border-4">Book Price</th>
-                 <th class="px-4 border-black rounded-lg border-4">Book ISbn</th>
-                <th class="px-4 border-black rounded-lg border-4">Action</th>
+                <th class="px-4 border-black rounded-lg border-4">Book Category Id</th>
+                <th class="px-4 border-black rounded-lg border-4">Book Category Name</th>
             </tr>
-            <tr v-for="(prod) in bookFound"  :key="prod">
-                <td class="px-4 border-black rounded-lg border-4">{{prod.book_id}}</td>
-                <td class="px-4 border-black rounded-lg border-4">{{prod.book_name}}</td>
-                <td class="px-4 border-black rounded-lg border-4">{{prod.author}}</td>
-                 <td class="px-4 border-black rounded-lg border-4">{{prod.price}}</td>
-                <td class="px-4 border-black rounded-lg border-4">{{prod.book_image}}</td>
-                <td class="px-4 border-black rounded-lg border-4">{{prod.book_isbn}}</td>
+            <tr v-for="(itemcategory) in statecategory.itemcategory"  :key="itemcategory">
+              <td class="px-4 border-black rounded-lg border-4">{{itemcategory.bookcategory_id}}</td>
+              <td class="px-4 border-black rounded-lg border-4">{{itemcategory.bookcategory_name}}</td>
             </tr>
         </table>
-        </div> -->
+        </div>
     </div>
       </div>
 </div>
@@ -166,9 +140,15 @@ const formData = reactive ({
     book_price:"",
     book_image:"",
     book_isbn:"",
-    //bookcategory_name:"",
+   // bookcategory_name:"",
     // addedone line extra as array
 });
+
+const formDatacategory=reactive({
+  bookcategory_id: "",
+  bookcategory_name: "",
+})
+
 const rules = {
     book_id:{ required,numeric},
     book_name:{ required,alpha},
@@ -176,38 +156,37 @@ const rules = {
     book_price:{ required,numeric },
     book_image:{ required,alpha },
     book_isbn:{ required,numeric },
-    //bookcategory_name:{ required},
+   // bookcategory_name:{ required},
 };
+
+const rulescategory={
+  bookcategory_id:{required,numeric},
+  bookcategory_name:{required},
+};
+
+
 const v$ = useVuelidate(rules,formData)
-// const submitForm = async () => {
-//     const result = await v$.value.$validate();
-//     if(result)
-//     {
-//         alert("successfully done");
-//     }
-//     else
-//     {
-//         alert("form is not submitted");
-//     }
-//     alert("successfully , form is submitted");
-// };
-//import { ref } from "vue";
-// how define multiple variable/ref in single reactive state
-// Access in html template
-// const book_id = ref("");
-// const book_name = ref("");
-// const book_author = ref("");
-// const book_image = ref("");
-// const book_price = ref("");
-// const book_isbn = ref("");
 let state = reactive({
   // allBooks: []
   items: [],
 });
+
+const vv$ = useVuelidate(rulescategory,formDatacategory)
+let statecategory = reactive({
+  itemcategory:[],
+});
+
+
 getalldata();
 async function  getalldata(){
     state.items = await $fetch("http://localhost:3001/book");
 }
+
+getalldatacategory();
+async function getalldatacategory(){
+  statecategory.itemcategory=await $fetch("http://localhost:3001/bookcontroller");
+}
+
 // 2nd part
 async function formSubmit() {
     const result = await v$.value.$validate();
@@ -230,16 +209,36 @@ async function formSubmit() {
     book_isbn: formData.book_isbn,
     //bookcategory_name: formData.bookcategory_name,
   };
+  const sampleDatacategory = {
+    bookcategory_id: formDatacategory.bookcategory_id,
+    bookcategory_name: formDatacategory.bookcategory_name,
+  };
   console.log("sample ",sampleData);
   const response = await $fetch("http://localhost:3001/book", {
     method: "POST",
     body: JSON.stringify(sampleData),
+    //  body: JSON.stringify(sampleDatacategory),
   }).then((res) =>{
  //console.log( alert("successfully done"));
   });
   getalldata();
+  //getalldatacategory();
   clearData();
+  //clearDatacategory();
+
+
+const responsee = await $fetch("http://localhost:3001/bookController", {
+    method: "POST",
+    body: JSON.stringify(sampleDatacategory),
+  }).then((res) =>{
+ //console.log( alert("successfully done"));
+  });
+ 
+  getalldatacategory();
+  clearDatacategory();
+
 }
+
 
 async function clearData() {
   formData.book_id = "";
@@ -250,6 +249,12 @@ async function clearData() {
   formData.book_isbn = "";
   //formData.bookcategory_name = "";
 }
+
+async function clearDatacategory() {
+  formDatacategory.bookcategory_id="";
+  formDatacategory.bookcategory_name="";
+}
+  
 
 //Delete Api
 async function deletespecificuser(book_id) {
@@ -299,27 +304,5 @@ async function editBookApi(book) {
         });
         console.log(this.bookFound);
     }
-    // bookFind(bookaddress)
-    // {
-    //     this.bookFound = this.formData.filter((e) => {
-    //         formData.book_id1 = bookaddress.toLocaleLowerCase();
-    //         formData.book_id2 = e.book_id.toLocaleLowerCase();
-    //         formData.book_name1 = bookaddress.toLocaleLowerCase();
-    //         formData.book_name2 = e.book_name.toLocaleLowerCase();
-    //         formData.author1 = bookaddress.toLocaleLowerCase();
-    //         formData.author2 = e.author.toLocaleLowerCase();
-    //         formData.price1 = bookaddress.toLocaleLowerCase();
-    //         formData.price2 = e.price.toLocaleLowerCase();
-    //         formData.book_image1 = bookaddress.toLocaleLowerCase();
-    //         formData.book_image2 = e.book_image.toLocaleLowerCase();
-    //         formData.book_isbn1 = bookaddress.toLocaleLowerCase();
-    //         formData.book_isbn = e.book_isbn.toLocaleLowerCase();
-    //         if(formData.book_id2.startsWith(formData.book_id1) || formData.book_name2.startsWith(formData.book_name1) || formData.author2.startsWith(formData.author1) || formData.price2.startsWith(formData.price1) || formData.book_image2.startsWith(formData.book_image1) || formData.book_isbn.startsWith(this.book_isbn1)){
-    //             console.log(e);
-    //             return e;
-    //         }
-    //     });
-    //     console.log(this.bookFound);
-    // }
 }
 </script>
